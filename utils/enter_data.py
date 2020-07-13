65d1cb1c59119c34dcdd6182440cc7a50492cda8
KleyLima <kley158@gmail.com>
2020-07-13 18:10:50 -0300

    Functions to: - Set parameters to obj - Read from memory (reversed) - Calc the hex value of value read

--- utils/enter_data.py
+++ utils/enter_data.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from utils.clear import clear
+from utils.hex import Hex
+from functools import reduce
 
 
 class Input:
@@ -8,7 +10,7 @@ class Input:
     @staticmethod
     def enter_data(rows, confirmation=False):
         """
-        Read the data stream from the input, aggregationg in a single list
+        Read the data stream from the input, aggregating in a single list
         :param rows: Number of rows that will be read from input
         :param confirmation: Flag if this is a confirmation call of the method
         :return: List of strings
@@ -18,3 +20,32 @@ class Input:
         stream = []
         [stream.extend(input("Type the {} line: ".format(turn + 1)).split()) for turn in range(rows)]
         return stream
+
+    @staticmethod
+    def set_parameters(obj):
+        """
+        A few things are happening here. Every class that call this function should have a "PARAMETERS" class attribute
+        that store the names of the parameters that are assigned by specif position of bytes in RAM memory.
+        For each name in PARAMETERS mighty exist a class attribute with the same name with the region of the memory
+        that points for the info needed.
+        :param obj: Obj that will assing the values
+        :return: None, its all about attribution
+        """
+        [setattr(obj, name_param, eval('{}{}'.format(obj.dump, getattr(obj, name_param.upper()))))
+         for name_param in obj.PARAMETERS]
+
+    @staticmethod
+    def concat_stream(stream):
+        """
+        Takes the data that is in a list of string an group in one single string.
+        The value saved in memory its REVERSED for the human readable concept, so its re-reversed now.
+        NOTE: Alsos reverse things that are not save reversed (rare cases), e.g filename, fat type.
+        :return: A String
+        """
+        stream.reverse() if isinstance(stream, list) else None
+        return reduce(lambda k, l: k + l, stream)
+
+    @staticmethod
+    def set_hex_values(obj):
+        [setattr(obj, f"{name_param}_hex", Hex(Input.concat_stream(getattr(obj, name_param))))
+         for name_param in obj.PARAMETERS]
--- utils/hex.py
+++ utils/hex.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
+
 class Hex:
-    
+
     def __init__(self, vl_hex):
         self.vl_hex = vl_hex
         self.vl_dec = self.to_decimal()
@@ -9,11 +10,12 @@ class Hex:
     def to_decimal(self):
         return int(f"0x{self.vl_hex}", 16)
 
-
     def __mul__(self, other):
         if isinstance(other, Hex):
             return Hex(format(self.vl_dec * other.vl_dec, 'X'))
 
+    def __repr__(self):
+        return f"Hex: {self.vl_hex}h Dec: {self.vl_dec}"
 
 # cmd = "echo 'obase=16;ibase=16;1.8*3' | bc"
 # out = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
@@ -24,4 +26,3 @@ no = Hex('a')
 print(na.vl_hex, na.vl_dec)
 res = na * no
 print(res.vl_dec, res.vl_hex)
-
